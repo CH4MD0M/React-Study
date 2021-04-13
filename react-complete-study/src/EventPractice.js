fb341e45307b8a4eb9c9ee3698b3836deb78f3e6
CH4MD0M <54847910+CH4MD0M@users.noreply.github.com>
2021-04-13 12:53:32 +0900

    Event(2)
    - form으로 여러개의 input 처리하기

--- react-complete-study/src/EventPractice.js
+++ react-complete-study/src/EventPractice.js
@@ -12,16 +12,21 @@ class EventPractice extends Component {
           type="text"
           name="message"
           placeholder="아무거나 입력해 보세요"
+          // value 값을 state.message로 지정
           value={this.state.message}
+          // 변화가 있을 때 다시 value 값을 state.message로 변경
           onChange={(e) => {
             // console.log(e);
             // console.log(e.target.value);
             this.setState({
+              // input(e.target)의 value를 state의 message 값으로 설정(setState)
               message: e.target.value,
             });
           }}
         />
         <button
+          // 버튼을 클릭하면 state에 있는 messaage를 알림창에 띄우고
+          // state의 message를 비운다.
           onClick={() => {
             alert(this.state.message);
             this.setState({
--- react-complete-study/test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function App() {
-  const name = "React";
-  return <div>{name === "React" && <h1>리액트 입니다.</h1>}</div>;
-}
-
-export default App;
