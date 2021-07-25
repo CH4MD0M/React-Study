853b7b89de373abbad4511c236d5419e9c828382
CH4MD0M <54847910+CH4MD0M@users.noreply.github.com>
2021-07-25 12:31:21 +0900

    리액트 key 예제

---
+++ react-book-study/Key/IterationSample.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+const IterationSample = () => {
+    const [names, setNames] = useState([
+        { id: 1, text: "눈사람" },
+        { id: 2, text: "얼음" },
+        { id: 3, text: "눈" },
+        { id: 4, text: "바람" },
+    ]);
+    const [inputText, setInputText] = useState("");
+    const [nextId, setNextId] = useState(5);
+
+    const onChange = (e) => {
+        setInputText(e.target.value);
+    };
+
+    // 요소 추가
+    const onClick = () => {
+        const nextNames = names.concat({ id: nextId, text: inputText });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText("");
+    };
+
+    // 요소 제거
+    const onRemove = (id) => () => {
+        const nextNames = names.filter((name) => name.id !== id);
+        console.log(nextNames);
+        setNames(nextNames);
+    };
+
+    const nameList = names.map((name) => (
+        <li key={name.id} onDoubleClick={onRemove(name.id)}>
+            {name.text}
+        </li>
+    ));
+
+    return (
+        <>
+            <input value={inputText} onChange={onChange} />
+            <button onClick={onClick}>추가</button>
+            <ul>{nameList}</ul>
+        </>
+    );
+};
+export default IterationSample;
