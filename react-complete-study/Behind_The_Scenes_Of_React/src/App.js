0d577c4daf1ad9f58d19ac844a9d63f333d90f5b
CH4MD0M <54847910+CH4MD0M@users.noreply.github.com>
2021-08-07 22:15:53 +0900

    Component Re-Evaluation

---
+++ react-complete-study/Behind_The_Scenes_Of_React/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
+import Button from "./components/UI/Button/Button";
+
+function App() {
+    const [showParagraph, setShowParagraph] = useState(false);
+
+    console.log("APP RUNNING");
+
+    const toggleParagraphHandler = () => {
+        setShowParagraph((preShowParagraph) => !preShowParagraph);
+    };
+
+    return (
+        <div className="app">
+            <h1>Hi there!</h1>
+            {/* {showParagraph && <p>This is new!</p>} */}
+            <DemoOutput show={false} />
+            <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+        </div>
+    );
+}
+
+export default App;
