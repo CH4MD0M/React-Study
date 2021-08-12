3137f6485272416170ca4a102a5126b6c4f8177c
CH4MD0M <54847910+CH4MD0M@users.noreply.github.com>
2021-08-12 15:41:36 +0900

    Forms & User Input_Part_4
    - add email input
    **will be changed by customHooks

--- react-complete-study/Forms_&_User_Input/src/components/SimpleInput.js
+++ react-complete-study/Forms_&_User_Input/src/components/SimpleInput.js
@@ -1,39 +1,42 @@
 import { useState } from "react";
 
-const SimpleInput = (props) => {
+const SimpleInput = () => {
     const [enteredName, setEnteredName] = useState("");
-    // 초기 화면에서도 state가 true로 되어있기 때문에 input에 내용을 작성했을 때를 판별해야 함.
     const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-    // const [formIsValid, setFormIsValid] = useState(false);
+
+    const [enteredEmail, setEnteredEmail] = useState("");
+    const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
 
     const enteredNameIsValid = enteredName.trim() !== "";
     const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+    const enteredEmailIsValid = enteredEmail.includes("@");
+    const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
+
     let formIsValid = false;
 
-    if (enteredNameIsValid) {
+    if (enteredNameIsValid && enteredEmailIsValid) {
         formIsValid = true;
     }
 
-    // form에 대한 처리
-    // useEffect(() => {
-    //     if (enteredNameIsValid) {
-    //         setFormIsValid(true);
-    //     } else {
-    //         setFormIsValid(false);
-    //     }
-    // }, [enteredNameIsValid]);
-
     const nameInputChangeHandler = (e) => {
         setEnteredName(e.target.value);
+    };
+    const emailInputChangeHandler = (e) => {
+        setEnteredEmail(e.target.value);
     };
 
     const nameInputBlurHandler = (e) => {
         setEnteredNameTouched(true);
     };
+    const emailInputBlurHandler = (e) => {
+        setEnteredEmailTouched(true);
+    };
 
     const formSubmissionHandler = (e) => {
         e.preventDefault();
         setEnteredNameTouched(true);
+        setEnteredEmailTouched(true);
 
         // input에 아무 입력도 하지 않았을 때
         if (!enteredNameIsValid) {
@@ -41,12 +44,19 @@ const SimpleInput = (props) => {
         }
 
         console.log(enteredName);
+
         setEnteredName("");
         setEnteredNameTouched(false);
+
+        setEnteredEmail("");
+        setEnteredEmailTouched(false);
     };
 
     // CSS Class Change
     const nameInputClasses = nameInputIsInvalid
+        ? "form-control invalid"
+        : "form-control ";
+    const emailInputClasses = emailInputIsInvalid
         ? "form-control invalid"
         : "form-control ";
 
@@ -63,6 +73,19 @@ const SimpleInput = (props) => {
                 />
                 {nameInputIsInvalid && (
                     <p className={"error-text"}>Name must Not be EMPTY!</p>
+                )}
+            </div>
+            <div className={emailInputClasses}>
+                <label htmlFor="email">Your E-mail</label>
+                <input
+                    type="email"
+                    id="email"
+                    onChange={emailInputChangeHandler}
+                    onBlur={emailInputBlurHandler}
+                    value={enteredEmail}
+                />
+                {emailInputIsInvalid && (
+                    <p className={"error-text"}>Please enter a valid email.</p>
                 )}
             </div>
             <div className="form-actions">
