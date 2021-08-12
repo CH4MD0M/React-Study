31908a8e66f03e63635608a24dd2d547bfec83e6
CH4MD0M <54847910+CH4MD0M@users.noreply.github.com>
2021-08-12 12:24:14 +0900

    Forms & User Input_Part_2
    When a input is losing focus

--- react-complete-study/Forms_&_User_Input/src/components/SimpleInput.js
+++ react-complete-study/Forms_&_User_Input/src/components/SimpleInput.js
@@ -7,8 +7,18 @@ const SimpleInput = (props) => {
     // 초기 화면에서도 state가 true로 되어있기 때문에 input에 내용을 작성했을 때를 판별해야 함.
     const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-    const nameInputChangehandler = (e) => {
+    const nameInputChangeHandler = (e) => {
         setEnteredName(e.target.value);
+    };
+
+    const nameInputBlurHandler = (e) => {
+        setEnteredNameTouched(true);
+
+        // input에 아무 입력도 하지 않았을 때
+        if (enteredName.trim() === "") {
+            setEnteredNameIsValid(false);
+            return;
+        }
     };
 
     const formSubmissionHandler = (e) => {
@@ -46,7 +56,8 @@ const SimpleInput = (props) => {
                     ref={nameInputRef}
                     type="text"
                     id="name"
-                    onChange={nameInputChangehandler}
+                    onChange={nameInputChangeHandler}
+                    onBlur={nameInputBlurHandler}
                     value={enteredName}
                 />
                 {nameInputIsInvalid && (
