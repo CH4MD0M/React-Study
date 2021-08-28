fcca75d7d6622bf5a0e9053025766d0decdf1bf2
ch4md0m <started677@gmail.com>
2021-08-28 23:37:02 +0900

    Header Section

--- slack-clone/src/App.js
+++ slack-clone/src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
     return (
         <div className="app">
-            <h1>slack clone</h1>
+            <Router>
+                <>
+                    <Switch>
+                        <Route path="/">
+                            <Header />
+                        </Route>
+                    </Switch>
+                </>
+            </Router>
         </div>
     );
 }
