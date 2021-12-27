bf378f358c7e66b5f2958489d7862bae2f197590
ch4md0m <started677@gmail.com>
2021-12-27 16:51:34 +0900

    React-rendering
    - map 메서드 사용 render.
    - 조건부(&&사용) component 출력

--- react-complete-study/03_React_Basics_&_Working_With_Components/src/components/NewExpense/NewExpense.js
+++ react-complete-study/03_React_Basics_&_Working_With_Components/src/components/NewExpense/NewExpense.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
+
     const saveExpenseDataHandler = (enterendExpenseData) => {
         const expenseData = {
             ...enterendExpenseData,
@@ -11,9 +13,23 @@ const NewExpense = (props) => {
         props.onAddExpense(expenseData);
     };
 
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    };
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    };
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {!isEditing && (
+                <button onClick={startEditingHandler}>Add new Expense</button>
+            )}
+            {isEditing && (
+                <ExpenseForm
+                    onSaveExpenseData={saveExpenseDataHandler}
+                    onCancel={stopEditingHandler}
+                />
+            )}
         </div>
     );
 };
