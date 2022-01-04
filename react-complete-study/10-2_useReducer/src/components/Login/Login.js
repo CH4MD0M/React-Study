487aa54a2171086d72396ae97a5632557f2d6651
ch4md0m <started677@gmail.com>
2022-01-04 16:15:08 +0900

    React-contextAPI

--- react-complete-study/10-2_useReducer/src/components/Login/Login.js
+++ react-complete-study/10-2_useReducer/src/components/Login/Login.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
+import AuthContext from "../store/auth-context";
 import Card from "../UI/Card/Card";
-import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+
+import classes from "./Login.module.css";
 
 const emailReducer = (state, action) => {
     if (action.type === "USER_INPUT") {
@@ -22,7 +24,8 @@ const passwordReducer = (state, action) => {
     }
     return { value: "", isValid: false };
 };
-const Login = (props) => {
+const Login = () => {
+    const authCtx = useContext(AuthContext);
     // const [enteredEmail, setEnteredEmail] = useState("");
     // const [emailIsValid, setEmailIsValid] = useState();
     // const [enteredPassword, setEnteredPassword] = useState("");
@@ -89,7 +92,7 @@ const Login = (props) => {
 
     const submitHandler = (event) => {
         event.preventDefault();
-        props.onLogin(emailState.value, passwordState.value);
+        authCtx.onLogin(emailState.value, passwordState.value);
     };
 
     return (
