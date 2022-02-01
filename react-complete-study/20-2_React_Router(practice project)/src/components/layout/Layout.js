4782f5be0b25bf0394643cbb3b58de0790f31122
ch4md0m <started677@gmail.com>
2022-02-01 23:01:10 +0900

    React-router(example)
    Practice Project

---
+++ react-complete-study/20-2_React_Router(practice project)/src/components/layout/Layout.js
@@ -0,0 +1,15 @@
+import MainNavigation from "./MainNavigation";
+
+// Css
+import classes from "./Layout.module.css";
+
+const Layout = (props) => {
+    return (
+        <>
+            <MainNavigation />
+            <main className={classes.main}> {props.children}</main>
+        </>
+    );
+};
+
+export default Layout;
